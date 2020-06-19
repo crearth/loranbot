a29fda7688846e5625a0fc0b4fb3d0e2938cb3f9
crearth <46131286+crearth@users.noreply.github.com>
2020-06-19 23:22:21 +0200

    add NASA API for mars command

--- loranbot.js
+++ loranbot.js
@@ -1,7 +1,8 @@
 const tmi = require('tmi.js');
+const fetch = require('node-fetch');
 require('dotenv').config();
 
-// Define configuration options
+// tmi.js options
 const opts = {
   connection: {
     secure: true,
@@ -16,6 +17,10 @@ const opts = {
     process.env.CHANNEL_NAME_TWO
   ]
 };
+
+// NASA api
+const API_KEY = process.env.NASA_API_KEY;
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
 
 // create client with the options
 const client = new tmi.client(opts);
@@ -46,6 +51,12 @@ function onMessageHandler (target, context, msg, self) {
       }
   }
 
+  // !mars
+  if (commandName === '!mars') {
+    client.say(target, `Gemiddelde temperatuur vandaag op Mars: ${vandaagTempCelcius}°C.`)
+    console.log(`* Executed ${commandName} with a temperature of ${vandaagTempCelcius}°C.`);
+  }
+
   // test
   if (commandName === '!test') {
     client.say(target, 'Ne bidon me water, uniek.');
@@ -58,6 +69,45 @@ function onMessageHandler (target, context, msg, self) {
 function scoreBidons () {
   const maxBidons = 5;
   return Math.floor(Math.random() * maxBidons) + 1;
+}
+
+// MARS FUNCTIONS
+
+let vandaagTemp
+let vandaagTempCelcius
+let vandaagSolIndex
+
+// laatste data verkrijgen
+getWeather().then(sols => {
+  vandaagSolIndex = sols.length - 1
+  displayVandaagTemp(sols)
+})
+
+// laatste data gemiddelde temperatuur verkrijgen
+function displayVandaagTemp(sols) {
+  const vandaagSol = sols[vandaagSolIndex]
+  vandaagTemp = vandaagSol.avTemp
+  vandaagTempCelcius = Math.floor((vandaagTemp - 32) * 5/9)
+}
+
+// temp Mars verkrijgen
+function getWeather () {
+  return fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+        const {
+          sol_keys,
+          validity_checks,
+          ...solData
+        } = data
+      return Object.entries(solData).map(([sol, data]) => {
+        return {
+          //maxTemp: data.AT.mx,
+          //minTemp: data.AT.mn,
+          avTemp: data.AT.av
+        }
+      })
+    })
 }
 
 // Called every time the bot connects to Twitch chat
